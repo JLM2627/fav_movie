8c66c5a2bcd13c1f0542cbc8355121dd27ad5fb6
Jayme Marshall <arcal619@gmail.com>
2017-10-30 21:28:29 -0400

    seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Favorite.destroy_all
+Movie.destroy_all
+
+
+
+Movie.create(title: 'big lebowski', genre: 'comedy', year: '1998', synopsis: 'blah blah blah', image: 'oops')
+Movie.create(title: 'little lebowski', genre: 'comedy', year: '1997', synopsis: 'blah blah blah', image: 'oops')
+Movie.create(title: 'middle lebowski', genre: 'comedy', year: '1995', synopsis: 'blah blah blah', image: 'oops')
+Movie.create(title: 'old lebowski', genre: 'comedy', year: '1958', synopsis: 'blah blah blah', image: 'oops')
+Movie.create(title: 'gnarly lebowski', genre: 'comedy', year: '1994', synopsis: 'blah blah blah', image: 'oops')  
+Movie.create(title: 'silly lebowski', genre: 'comedy', year: '1968', synopsis: 'blah blah blah', image: 'oops')
+
+Favorite.create(movie_id: 1)
+Favorite.create(movie_id: 2)
+Favorite.create(movie_id: 3)
+Favorite.create(movie_id: 4)
+Favorite.create(movie_id: 5)
